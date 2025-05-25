f4e1f2c99e28206832d994529d0bf9431ea05459
Pablo da Silva Bezerra <Pdsbezerra1316@gmail.com>
2025-05-25 16:40:18 -0300

    add dir. docs and remove dir. dist

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'production',
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js',
     },
     //devtool:'source-map',
@@ -29,16 +29,16 @@ module.exports = {
             {
                 test: /\.svg$/i,
                 type: 'asset/resource',
-                include: path.resolve(__dirname, 'src/assets'),
+                include: path.resolve(__dirname, 'src/images'),
             },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './dist/index.html',
+            template: './docs/index.html',
         }),
         new HtmlWebpackPlugin({
-            template: './dist/index.html',
+            template: './docs/index.html',
         }),
         new CopyWebpackPlugin({
             patterns: [
@@ -47,7 +47,7 @@ module.exports = {
         })
     ],
     devServer: {
-        static: './dist',
+        static: './docs',
         port: 5000,
         open: true,
         host: '0.0.0.0',
